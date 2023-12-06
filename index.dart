b0687aa6af599e88207c2b1c118914bf5b7f4790
Macbook <macbook@Macbooks-MacBook-Air.local>
2023-12-06 21:24:41 +0545

    hello

---
+++ index.dart
@@ -0,0 +1,13 @@
+//Write a Program to check if the number is odd or even.
+void main()
+{
+  // Replace 5 with any number you want to check
+  int number = 5;
+
+  if (number % 2 == 0) {
+    print('$number is even.');
+  } else {
+    print('$number is odd.');
+  }
+
+}
